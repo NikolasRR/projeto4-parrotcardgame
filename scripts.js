061b81657d972e8c1f462e466f73da9f9c326fb5
NikolasRR <nkls99@gmail.com>
2022-02-04 16:50:53 -0300

    Gifs aleatórios e animação CSS desenvolvidos

--- scripts.js
+++ scripts.js
@@ -9,21 +9,38 @@ function perguntarNumeroDeCartas () {
     adicionarXCartas(numeroDeCartas);
 }
 
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
 function adicionarXCartas (x) {
     const elementoDasCartas = document.querySelector(".cartas");
-    let gifs = "bobrossparrot.gif";
+    let allGifs = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicornparrot"];
+    let gifs = [];
+    for (i = 0; i < x / 2; i++) {
+        gifs.push(allGifs[i]);
+        gifs.push(allGifs[i]);
+    }
+    gifs.sort(comparador);
     for (i = 0; i < x; i++) {
         elementoDasCartas.innerHTML += 
         `<div class="carta" onclick="virarCarta(this);">
-            <div class="frente face visivel"><img src="/Gifs e Imgs/front.png"></div>
-            <div class="verso face escondida"><img src="/Gifs e Imgs/${gifs}"></div>
+            <div class="face"><img src="/Gifs e Imgs/front.png"></div>
+            <div class="face naoVisivel"><img src="/Gifs e Imgs/${gifs[i]}.gif"></div>
         </div>`;
     }
 }
 
 
 function virarCarta (estaCarta) {
-    const faceVisivel = estaCarta.querySelector(".visivel");
-    const faceEscondida = estaCarta.querySelector(".escondida");
+    const faceVisivel = estaCarta.querySelector(".carta div:first-child");
+    const faceEscondida = estaCarta.querySelector(".carta div:last-child");
 
+    if (faceVisivel.classList.contains("naoVisivel")) {
+        faceVisivel.classList.remove("naoVisivel");
+        faceEscondida.classList.add("naoVisivel");
+    } else {
+        faceVisivel.classList.add("naoVisivel");
+        faceEscondida.classList.remove("naoVisivel");
+    }
 }
